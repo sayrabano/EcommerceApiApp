98133ad63c6d6c68cbd89767f4edfbda2562681e
sayrabano <87492998+sayrabano@users.noreply.github.com>
2023-07-01 20:06:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const port = process.env.port ||8000;
 const db = require('./config/mongoose')
 const bodyParser = require('body-parser')
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+    );
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
+    next();
+  });
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
