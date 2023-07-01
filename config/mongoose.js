3296566768451dd408ef05a4348092dbabc2d473
sayrabano <87492998+sayrabano@users.noreply.github.com>
2023-07-01 20:07:18 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,6 @@
 //require library
 const mongoose = require("mongoose");
 
-//connect to the databse
 mongoose.connect('mongodb+srv://sayrabano8888:mdOZ2NqcyHBeFGBe@cluster0.zwczm6d.mongodb.net/?retryWrites=true&w=majority',{
     useNewUrlParser:true
 })
